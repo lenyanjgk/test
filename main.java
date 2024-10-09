27b988eae46af72da3f3543465db9d79a2fa4d06
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2024-10-09 21:07:10 +0800

    init

---
+++ main.java
@@ -0,0 +1,12 @@
+/**
+ * @Program: java1   //名称
+ * @ClassName : main  //类名
+ * @Author: lenyan   /作者
+ * @Date: 2024/10/9  21:05
+ **/
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
